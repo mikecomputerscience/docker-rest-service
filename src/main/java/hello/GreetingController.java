9e1517bc5512d358ccab114e695942a93a119263
Ligang Ye <mikecomputerscience@gmail.com>
2018-09-05 15:44:50 -0700

    Changed to restful api

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,10 +1,10 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
@@ -13,11 +13,16 @@ public class GreetingController {
 
     @RequestMapping("/")
     public String index() {
-        return "test";
+        return "index";
     }
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting() {
+        return new Greeting(counter.incrementAndGet(), String.format(template, "World"));
+    }
+
+    @RequestMapping("/greeting/{name}")
+    public Greeting greeting(@PathVariable("name") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
